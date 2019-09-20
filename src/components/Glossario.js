495ab696827d1eca07ec6d4a16b1c572d13f12aa
Jefferson <jeffersonbds_barros@hotmail.com>
2019-09-20 17:46:11 -0300

    Tela visualizar termo

--- src/components/Glossario.js
+++ src/components/Glossario.js
@@ -6,47 +6,37 @@ import {Actions} from 'react-native-router-flux';
 const DATA = [
   {
     id: '1',
-    title: 'Link do Vídeo 1',
+    title: 'Homenagem Pais e Estudantes 2017',
+    link: 'https://www.youtube.com/watch?v=kq8xXOHNX1E'
   },
   {
     id: '2',
-    title: 'Link do Vídeo 2',
-  },
-  {
-    id: '3',
-    title: 'Link do Vídeo 3',
-  },
-  {
-    id: '4',
-    title: 'Link do Vídeo 4',
-  },
-  {
-    id: '5',
-    title: 'Link do Vídeo 5',
-  },
-  {
-    id: '6',
-    title: 'Link do Vídeo 6',
-  },
+    title: 'Institucional',
+    link: 'https://www.youtube.com/watch?v=I0iG0ktdCgU'
+  }
 ];
 
-function Item({title}) {
-  return (
-    <View style={styles.item}>
-      <TouchableOpacity onPress={() => {Actions.visualizaTermo()}} underlayColor="white">
-        <Text style={styles.title}>{title}</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
 class Glossario extends Component{
+
+  renderizaLinhas({item}){
+    return(
+      <View style={styles.item}>
+        <TouchableOpacity
+          onPress={() => {Actions.visualizaTermo({linkVideo: item.link, title: item.title})}}
+          underlayColor="white"
+        >
+          <Text style={styles.title}>{item.title}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   render(){
     return(
       <SafeAreaView style={styles.container}>
         <FlatList
           data={DATA}
-          renderItem={({item}) => <Item title={item.title} />}
+          renderItem={({item}) => this.renderizaLinhas({item}) }
           keyExtractor={item => item.id}
         />
       </SafeAreaView>
@@ -64,9 +54,10 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+    borderRadius: 8
   },
   title: {
-    fontSize: 30,
+    fontSize: 22,
     textAlign: 'center',
     color: '#fff'
   },
--- src/components/VisualizaTermo.js
+++ src/components/VisualizaTermo.js
@@ -1,16 +1,81 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+import YouTube from 'react-native-youtube';
+import {Actions} from 'react-native-router-flux';
 
 class VisualizaTermo extends Component{
   render(){
+
+    const link = this.props.linkVideo;
+    const video = link.split("=");
+
     return(
-      <Text> Visualiza Termo </Text>
+      <View style={styles.containerPrincipal}>
+        <View style={styles.containerVideo}>
+
+          <YouTube
+            videoId={video[1]}
+            play
+            apiKey="AIzaSyCl8plPNRCkljn0qphfdxMltbBbesPY2kU"
+            onReady={e => this.setState({ isReady: true })}
+            onChangeState={e => this.setState({ status: e.state })}
+            onChangeQuality={e => this.setState({ quality: e.quality })}
+            onError={e => this.setState({ error: e.error })}
+            style={styles.blocoYoutube}
+          />
+
+        </View>
+
+        <View style={styles.containerInformacoes}>
+          <Text style={styles.titulos}> Produzido por: </Text>
+          <Text style={styles.nomes}> Maria Patricia Lourenço Barros </Text>
+
+          <Text style={styles.titulos}> Termo apresentado: </Text>
+          <Text style={styles.nomes}> {this.props.title} </Text>
+
+          <Text style={styles.titulos}> Descrição: </Text>
+          <Text style={styles.nomes}>
+            Texto é um conjunto de palavras e frases encadeadas que permitem interpretação e transmitem uma mensagem.
+            É qualquer obra escrita em versão original e que constitui um livro ou um documento escrito.
+            Um texto é uma unidade linguística de extensão superior à frase.
+          </Text>
+        </View>
+
+        <Button title="Voltar" onPress={() => {Actions.pop()}} color="#359830" />
+
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  containerPrincipal: {
+    flex: 1
+  },
+  containerVideo: {
+    flex: 3
+  },
+  blocoYoutube: {
+    alignSelf: 'stretch',
+    height: 200
+  },
+  containerInformacoes: {
+    flex: 5
+  },
+  titulos: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#359830',
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  nomes: {
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
 });
 
 export default VisualizaTermo;
