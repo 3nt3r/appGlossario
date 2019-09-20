9fdee5feef9b1351b53fffb51d43448fd66db899
Jefferson <jeffersonbds_barros@hotmail.com>
2019-09-20 12:51:09 -0300

    Telas do projeto

---
+++ src/components/Glossario.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Glossario extends Component{
+  render(){
+    return(
+        <View>
+          <Text> Glossário </Text>
+        </View>
+    );
+  }
+}
+
+export default Glossario;
---
+++ src/components/TelaInicial.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class TelaInicial extends Component{
+  render(){
+    return(
+        <View>
+          <Text> Tela Inicial </Text>
+        </View>
+    );
+  }
+}
+
+export default TelaInicial;
