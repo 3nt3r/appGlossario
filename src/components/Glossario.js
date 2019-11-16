72707cd522ef249e9a20bd5e5ad32524441da53d
Ruben <rubenjosma11@gmail.com>
2019-11-16 19:57:59 -0300

    Corrigindo Problemas

--- src/components/Glossario.js
+++ src/components/Glossario.js
@@ -24,16 +24,30 @@ class Glossario extends Component{
         this.setState({dados: [{tipo: 1, mensagem: "Nenhum termo encontrado."}], carregamento: true});
       }else{
         let vetorEmOrdemAlfabetica = Object.values(snapshot.val()).sort((a, b) => {
-          if (a.termo > b.termo)
-            return 1
-          else if(a.termo < b.termo)
-            return -1
+          termoASemAcento = a.termo.replace(a.termo[0], this.removerAcento(a.termo[0]));
+          termoBSemAcento = b.termo.replace(b.termo[0], this.removerAcento(b.termo[0]));
+          if (termoASemAcento > termoBSemAcento)
+            return 1;
+          else if(termoASemAcento < termoBSemAcento)
+            return -1;
           else
-            return 0
+            return 0;
         });
         this.setState({dados: vetorEmOrdemAlfabetica, carregamento: true});
       }
     });
+  }
+
+  removerAcento(letraComAcento){
+    var letraMaiuscula = letraComAcento.toUpperCase();
+    var com_acento = "ÁÀÂÃÉÊÍÔÓÚ";
+    var sem_acento = "AAAAEEIOOU";
+    for(i=0;i<com_acento.length;i++){
+      if(letraMaiuscula == com_acento[i]){
+        return sem_acento[i];
+      }
+    }
+    return letraMaiuscula;
   }
 
   renderizaLinhas({item}, index){
@@ -44,12 +58,12 @@ class Glossario extends Component{
         </View>
       );
     }else{
-      aux = false
+      aux = false;
       if(index == 0){
-        letra = item.termo[0];
-        aux = true
-      }else if(item.termo[0] != letra){
-        aux = true
+        letra = this.removerAcento(item.termo[0]);
+        aux = true;
+      }else if(this.removerAcento(item.termo[0]) != letra){
+        aux = true;
         letra = item.termo[0];
       }
       return(
@@ -78,8 +92,8 @@ class Glossario extends Component{
 
   pesquisar(text){
     this.setState({ termoPesquisado: text });
-    let textoParaPesquisa = text.toLowerCase()
-    let termoParaComparacao = ""
+    let textoParaPesquisa = text.toLowerCase();
+    let termoParaComparacao = "";
     let resultados = [];
 
     this.state.dados.forEach(elemento => {
@@ -87,7 +101,7 @@ class Glossario extends Component{
         termoParaComparacao += elemento.termo[i].toLowerCase();
       }
       if(termoParaComparacao === (textoParaPesquisa)){
-        resultados.push(elemento)
+        resultados.push(elemento);
       }
       termoParaComparacao = "";
     });
@@ -106,7 +120,7 @@ class Glossario extends Component{
           <FlatList
             data={this.state.termoPesquisado == "" ? this.state.dados : this.state.listaPesquisada}
             extraData={this.state}
-            renderItem={({item}, index) => this.renderizaLinhas({item}, index)}
+            renderItem={({item, index}) => this.renderizaLinhas({item}, index)}
             keyExtractor={item => item.video}
           />
         </ScrollView>
